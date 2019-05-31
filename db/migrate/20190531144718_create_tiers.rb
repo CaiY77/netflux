ba940d3a516702df79b2aa4697d5af9abbf593cb
Cai Yang <caih.yang@gmail.com>
2019-05-31 10:48:20 -0400

    db setup

---
+++ db/migrate/20190531144718_create_tiers.rb
@@ -0,0 +1,10 @@
+class CreateTiers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tiers do |t|
+      t.string :plan
+      t.string :price
+
+      t.timestamps
+    end
+  end
+end
